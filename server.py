54264da574be1cdad454eaed40b6bc61348bc27f
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2019-04-11 17:11:08 -0400

    E2E flow working on shallow tests

--- server.py
+++ server.py
@@ -9,9 +9,9 @@ TAG = "[TagginToolWebServer]"
 
 def dataset_update_worker(dataset_addr, update_query_q, stop_event):
     tag = '[UpdateWorker]'
-    d = pd.read_hdf(dataset_addr)
     while not stop_event.is_set():
         query = update_query_q.get()
+        d = pd.read_hdf(dataset_addr)
         print("{} Got new query. Class: {}, Image {}, Use: {}".format(tag,
                                                                       query['class'],
                                                                      query['image_name'],
@@ -19,6 +19,7 @@ def dataset_update_worker(dataset_addr, update_query_q, stop_event):
 
         d.loc[(d['class'] == query['class']) & (d['image_name'] == query['image_name']), 'use_photo'] = int(query['use_photo'])
         d.to_hdf(dataset_addr, 'main')
+    print("Stopping updater worker ...")
 
 def resolve_image_name(df, max_count, starting_class=None):
     while True:
@@ -28,6 +29,7 @@ def resolve_image_name(df, max_count, starting_class=None):
         not_tagged_yet = df[(df['class'] == starting_class) & (df['use_photo']
                                                               == -1)]
         if not_tagged_yet.empty:
+            print("\tAll images of class {} are already tagged...".format(starting_class))
             starting_class = None
             continue
 
@@ -35,13 +37,14 @@ def resolve_image_name(df, max_count, starting_class=None):
                                                               != -1)]
 
         if max_count != -1 and tagged_already.count()[0] >= max_count:
+            print("\tClass {} has already {} tagged images ...".format(starting_class, max_count))
             starting_class = None
             continue
 
         return not_tagged_yet.sample(1)[['class', 'image_name']].values[0]
 
 def all_images_tagged(df):
-    return not df[df.use_photo != -1].empty
+    return df[df.use_photo == -1].empty
 
 def all_classes_has_min_items_tagged(df, count):
     tagged_by_class = df[df.use_photo != -1].groupby('class')
@@ -49,39 +52,72 @@ def all_classes_has_min_items_tagged(df, count):
 
 class ProvidePhotoHandler(RequestHandler):
 
+    def check_origin(self, origin):
+        return True
+
     def initialize(self, dataset_addr, images_url, max_count=30):
-        self.df = pd.read_hdf(dataset_addr)
-        self.max_count = -1 if all_classes_has_min_items_tagged(self.df,
-                                                                max_count) else max_count
         self.images_url = images_url
+        self.dataset_addr = dataset_addr
+        self.max_count = max_count
 
     def get(self):
         _class = self.get_body_argument("class", None)
 
-        if all_images_tagged(self.df):
+        print("Finding new photo ... ")
+        df = pd.read_hdf(self.dataset_addr)
+        max_count = -1 if all_classes_has_min_items_tagged(df, self.max_count) else self.max_count
+        if all_images_tagged(df):
+            print("All images are already tagged ...")
             self.write({'all_images_tagged': 1})
         else:
-            next_class, next_image = resolve_image_name(self.df, self.max_count, _class)
-            self.write({'image_path': "{}/{}/{}".format(self.images_url,
+            next_class, next_image = resolve_image_name(df, max_count, _class)
+            del df
+            img_addr = "{}/{}/{}".format(self.images_url,
                                                         next_class,
-                                                        next_image),
-                        'all_images_tagged': 0})
+                                                        next_image)
+            print("Returning path: {}".format(img_addr))
+            '''
+            self.set_header('Content-Type', 'image/png')
+            with open(img_addr, 'rb') as f:
+                self.write(f.read())
+            '''
+            self.set_header('Access-Control-Allow-Origin', "*")
+            self.write({"all_images_tagged": 0, "image_url": img_addr,
+                        "class": next_class, "image_name": next_image})
 
 
 class EnqueueAnnotationHandler(RequestHandler):
 
+    def check_origin(self, origin):
+        return True
+
+    '''
     def initialize(self, queue):
         self.queue = queue
+    '''
+
+    def initialize(self, dataset_addr):
+        self.dataset_addr = dataset_addr
 
     def get(self):
-        _class = self.get_body_argument("class")
-        image_name = self.get_body_argument("image_name")
-        use_photo = self.get_body_argument("use_photo")
+        _class = self.get_argument("class")
+        image_name = self.get_argument("image_name")
+        use_photo = self.get_argument("use_photo")
+        d = pd.read_hdf(self.dataset_addr)
+        print("Annotating Class: {}, Image name: {}, Use photo: {}".format(_class, image_name, use_photo))
+        d.loc[(d['class'] == _class) & (d['image_name'] == image_name), 'use_photo'] = int(use_photo)
+        d.to_hdf(self.dataset_addr, 'main')
+        del d
+
+        '''
         print("Enqueing. Class: {}, Image name: {}, Use photo: {}".format(_class, image_name, use_photo))
         self.queue.put({'class': _class, 'image_name': image_name, 'use_photo':
                       use_photo})
+        '''
+
+        self.set_header('Access-Control-Allow-Origin', "*")
         self.set_status(200)
-        return
+        #return
 
 
 def main(images_url, dataset_path, max_count=30):
@@ -93,17 +129,20 @@ def main(images_url, dataset_path, max_count=30):
         (r"/get_photo", ProvidePhotoHandler,
          dict(dataset_addr=dataset_path,images_url=images_url,
                                                   max_count=max_count)),
-        (r"/save", EnqueueAnnotationHandler, dict(queue=update_query_q))
+        #(r"/save", EnqueueAnnotationHandler, dict(queue=update_query_q))
+        (r"/save", EnqueueAnnotationHandler, dict(dataset_addr=dataset_path))
     ])
 
+    '''
     update_worker_stopper = Event()
     Thread(target=dataset_update_worker, args=(dataset_path, update_query_q,
                                                update_worker_stopper)).start()
+    '''
 
     app.listen(5000, '0.0.0.0')
     print("{} Server started ...".format(TAG))
     IOLoop.current().start()
-    update_worker_stopper.set()
+    #update_worker_stopper.set()
 
 
 if __name__ == "__main__":
